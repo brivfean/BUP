d4a66b96592d25a8f8ab998eaf2b94c065a73b88
brivfean <128435123+brivfean@users.noreply.github.com>
2023-06-05 14:15:45 -0600

    Base
    Se agrego la base del codigo

--- src/bup/BUP.java
+++ src/bup/BUP.java
@@ -5,6 +5,11 @@
  */
 package bup;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
 /**
  *
  * @author bruni
@@ -14,8 +19,52 @@ public class BUP {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    static boolean existenErrores = false;
+    
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        ejecutarPrompt();
+    }
+    
+    private static void ejecutarPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+            System.out.print(">>> ");
+            String linea = reader.readLine();
+            if(linea == null) break; // Presionar Ctrl + D
+            ejecutar(linea);
+            existenErrores = false;
+        }
+    }
+
+    private static void ejecutar(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        /*for(Token token : tokens){
+            System.out.println(token);
+        }*/
+
+        Parser parser = new Parser(tokens);
+        parser.parse();
+    }
+
+    /*
+    El método error se puede usar desde las distintas clases
+    para reportar los errores:
+    Interprete.error(....);
+     */
+    static void error(int linea, String mensaje){
+        reportar(linea, "", mensaje);
+    }
+
+    private static void reportar(int linea, String donde, String mensaje){
+        System.err.println(
+                "[linea " + linea + "] Error " + donde + ": " + mensaje
+        );
+        existenErrores = true;
     }
     
 }
